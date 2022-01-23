16ec337976c0622bcdfcb44fd040cd87e0945216
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2022-01-23 15:35:08 +0300

    feat: add welcome page

---
+++ client/src/pages/Welcome.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "antd";
+
+import "../styles/welcome.scss";
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div className="welcome-page">
+        <h2 className="welcome-page__game-name">Guess Word</h2>
+        <img
+          className="welcome-page__app-logo"
+          src="./img/app-logo.png"
+          alt="Guess Word"
+        />
+        <div className="welcome-page__title">Угадай слово+</div>
+        <div className="welcome-page__description">
+          Игра угадай слово - это следующая итерация знаменитой игры «Крокодил»,
+          где Вам необходимо объяснить слово используя слова начинающиеся с
+          одной случайной буквы.
+        </div>
+        <Link to="/settings">
+          <Button type="primary" shape="round" block>
+            Ок, погнали
+          </Button>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
