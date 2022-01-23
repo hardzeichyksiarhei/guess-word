e1055f48712b0b65e13c145287b44957e88a3b11
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2022-01-23 20:11:40 +0300

    feat: add form

--- client/src/pages/Connect.tsx
+++ client/src/pages/Connect.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Button, Form, Input } from "antd";
 
 const Connect: React.FC = () => {
-    return (
-        <div>
-            Connect to game
-        </div>
-    );
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  return (
+    <Form layout="vertical" onFinish={onFinish}>
+      <Form.Item label="Как будет называться игра?">
+        <Input placeholder="Введите название игры..." />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary">Submit</Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default Connect;
--- client/src/pages/Create.tsx
+++ client/src/pages/Create.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Button, Form, Input } from "antd";
+
+import "../styles/create.scss";
 
 const Create: React.FC = () => {
-    return (
-        <div>
-            Create game
-        </div>
-    );
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  return (
+    <Form className="create-form" layout="vertical" onFinish={onFinish}>
+      <Form.Item label="Как будет называться игра?">
+        <Input placeholder="Введите название игры..." />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary">Создать</Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default Create;
