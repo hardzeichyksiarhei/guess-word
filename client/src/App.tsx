9660c16ba7e1c76f3af3a2607ee29dde45bac728
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2022-01-23 16:04:37 +0300

    feat: add default layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,35 @@
 import { Route, Routes } from "react-router-dom";
 
 import EmptyLayout from "./layouts/EmptyLayout";
+import DefaultLayout from "./layouts/DefaultLayout";
 import Welcome from "./pages/Welcome";
+import Create from "./pages/Create";
+import Connect from "./pages/Connect";
+import Profile from "./pages/Profile";
 
 function App() {
-  return <div className="app">
-    <Routes>
-      <Route path="/" element={<EmptyLayout />}>
-        <Route index element={<Welcome />} />
-      </Route>
-    </Routes>
-  </div>;
+  const publicRoutes = (
+    <Route path="/" element={<EmptyLayout />}>
+      <Route index element={<Welcome />} />
+      <Route path="create" element={<Create />} />
+      <Route path="connect" element={<Connect />} />
+    </Route>
+  );
+
+  const privateRoutes = (
+    <Route path="/" element={<DefaultLayout />}>
+      <Route index element={<Profile />} />
+    </Route>
+  );
+
+  return (
+    <div className="app">
+      <Routes>
+        {publicRoutes}
+        {privateRoutes}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
