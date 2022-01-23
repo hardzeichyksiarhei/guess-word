ded5683d7d3eafa07c9223c99d57abb2a69eae41
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2022-01-23 15:15:07 +0300

    feat: add app style

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
+import "./styles/app";
+
 import App from "./App";
 
 ReactDOM.render(
